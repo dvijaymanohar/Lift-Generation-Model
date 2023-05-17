8cbc48a8c4197655a7a8317e91395bdea22b2930
Vijaya Manohar Dogiparthi <dvijaymanohar@gmail.com>
2023-05-17 19:06:26 +0200

    Formulas to calculate the various physical quantities added.

--- src/lift_gen.c
+++ src/lift_gen.c
@@ -48,41 +48,37 @@ struct parameter_uncertainties {
   struct fluid_properties fluid_cfg;   // Fluid properties.
 };
 
-static void load_parameters(struct parameter_uncertainties *parameters) {
+static void load_parameters(struct parameter_uncertainties *const parameters) {
   // Pa, +/- 1000 Pa (source:
   // https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html)
-  double empiricalPitotPressureValues[] = {
+  double empirical_pressure_values[] = {895300, 896100, 898548, 900108,
+                                        902000, 904167}; // atmospheric pressure
+
+  double empirical_pitot_pressure_values[] = {
       895500, 896500, 898648,
       900200, 902020, 904567}; // pressure measured by Pitot tube (Pa)
 
-  // Pa, +/- 1000 Pa (source:
-  // https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html)
-  double empiricalPressureValues[] = {895300, 896100, 898548, 900108,
-                                      902000, 904167}; // atmospheric pressure
-
   // deg C, +/- 1 deg C (source:
   // https://www.engineeringtoolbox.com/air-properties-d_156.html)
-  double empiricalTemperatureValues[] = {278.48, 279.54, 281.65,
-                                         283.14, 283.56, 284.78};
+  double empirical_temperature_values[] = {278.48, 279.54, 281.65,
+                                           283.14, 283.56, 284.78};
   // ambient temperature (Kelvin)
 
-  // %, +/- 5% (source:
-  // https://www.engineeringtoolbox.com/humidity-ratio-d_585.html)
-  double empiricalHumidityValues[] = {0.34, 0.38, 0.41,
-                                      0.54, 0.6,  0.65}; // relative humidity
+  double empirical_humidity_values[] = {0.34, 0.38, 0.41,
+                                        0.54, 0.6,  0.65}; // relative humidity
 
   parameters->pt_cfg.pitot_pressure = libUncertainDoubleDistFromSamples(
-      empiricalPitotPressureValues,
-      sizeof(empiricalPitotPressureValues) / sizeof(double));
+      empirical_pitot_pressure_values,
+      sizeof(empirical_pitot_pressure_values) / sizeof(double));
   parameters->fluid_cfg.pressure = libUncertainDoubleDistFromSamples(
-      empiricalPressureValues,
-      sizeof(empiricalPressureValues) / sizeof(double));
+      empirical_pressure_values,
+      sizeof(empirical_pressure_values) / sizeof(double));
   parameters->fluid_cfg.temperature = libUncertainDoubleDistFromSamples(
-      empiricalTemperatureValues,
-      sizeof(empiricalTemperatureValues) / sizeof(double));
+      empirical_temperature_values,
+      sizeof(empirical_temperature_values) / sizeof(double));
   parameters->fluid_cfg.humidity = libUncertainDoubleDistFromSamples(
-      empiricalHumidityValues,
-      sizeof(empiricalHumidityValues) / sizeof(double));
+      empirical_humidity_values,
+      sizeof(empirical_humidity_values) / sizeof(double));
 
   parameters->airfoil_cfg.chord_length =
       libUncertainDoubleUniformDist(1.0, 1.01);
@@ -99,12 +95,13 @@ static void load_parameters(struct parameter_uncertainties *parameters) {
 }
 
 // Function to calculate the lift coefficient
-static double calculate_lift_coefficient(struct airfoil_geometry *af_cfg) {
-  double lift_coefficient;
-  double camber_line;
-  double dcp;
-  double theta;
-  double thickness_distribution;
+static double
+calculate_lift_coefficient(const struct airfoil_geometry *const af_cfg) {
+  double lift_coefficient = 0;
+  double camber_line = 0;
+  double dcp = 0;
+  double theta = 0;
+  double thickness_distribution = 0;
 
   // Calculate camber line
   camber_line = af_cfg->chord_length * af_cfg->camber / 2.0;
@@ -112,7 +109,13 @@ static double calculate_lift_coefficient(struct airfoil_geometry *af_cfg) {
   // Calculate angle in radians
   theta = af_cfg->angle_of_attack * PI / 180.0;
 
-  // Calculate thickness distribution
+  // The formula for calculating the thickness distribution of an airfoil is as
+  // follows: t = (thickness / 0.2) * [0.2969 * sqrt(x / c) - 0.1260 * (x / c) -
+  // 0.3516 * (x / c) ^ 2 + 0.2843 * (x / c) ^ 3 - 0.1015 * (x / c) ^ 4] t is
+  // the airfoil thickness at a given point along the chord length (x). c is the
+  // chord length of the airfoil. x is the distance along the chord length from
+  // the leading edge to the desired point.
+
   thickness_distribution =
       (af_cfg->thickness / 0.2) *
       (0.2969 * sqrt(af_cfg->chord_length) - 0.126 * af_cfg->chord_length -
@@ -120,10 +123,15 @@ static double calculate_lift_coefficient(struct airfoil_geometry *af_cfg) {
        0.2843 * pow(af_cfg->chord_length, 3.0) -
        0.1015 * pow(af_cfg->chord_length, 4.0));
 
-  // Calculate pressure coefficient distribution
+  // The formula for calculating the Drag Coefficient at Zero Lift Pitching
+  // Moment. dcp = 2.0 * thickness_distribution / chord_length;
+
   dcp = 2.0 * thickness_distribution / af_cfg->chord_length;
 
-  // Calculate lift coefficient
+  // The formula for calculating the lift_coefficient
+  // lift_coefficient = (2.0 * PI * (camber_line / chord_length) + DCP) *
+  // sine(angle)
+
   lift_coefficient =
       (2.0 * PI * (camber_line / af_cfg->chord_length) + dcp) * sin(theta);
 
@@ -131,8 +139,8 @@ static double calculate_lift_coefficient(struct airfoil_geometry *af_cfg) {
 }
 
 // Function to calculate lift force using the Bernoulli equation
-static double calc_lift_force(double cl, double rho, double v, double chord,
-                              double length) {
+static double calc_lift_force(const double cl, const double rho, const double v,
+                              const double chord, const double length) {
   // Bernoulli's equation:
   // lift_force = Lift-Coeff * WingArea * .5 * density * Velocity ^ 2
 
@@ -145,12 +153,22 @@ static double calc_lift_force(double cl, double rho, double v, double chord,
 // humidity and elevation
 static double calc_air_density(const double temperature, const double pressure,
                                const double humidity, const double elevation) {
-  const double temperatureLapseRate = 0.0065; // K/m
+  const double temperature_lapse_rate = 0.0065; // K/m
+  const double gas_constant = 287.05;
 
-  double T = temperature - temperatureLapseRate * elevation;
-  double RH = humidity / 100.0;
-  double e = RH * 6.112 * exp((17.67 * T) / (T + 243.5));
-  double air_density = (pressure * 100) / (287.05 * T) - (e * 2.1674) / T;
+  double T = temperature - temperature_lapse_rate * elevation;
+  double relative_humidity = humidity / 100.0;
+
+  // The formula to calculate the vapor pressure of water vapor (e)
+  // e = RH * (6.112 * exp((17.67 * T) / (T + 243.5)))
+
+  double e = relative_humidity * 6.112 * exp((17.67 * T) / (T + 243.5));
+
+  // Formula to calculate the air density, the ideal gas law in combination with
+  // the relative humidity and the barometric formula can be used ρ = (P / (Gas
+  // Constant * T)) * (e * 2.1674) / T)
+  double air_density = (pressure * 100) / (gas_constant * T) - (e * 2.1674) / T;
+
   return air_density;
 }
 
@@ -166,7 +184,9 @@ static double calc_wind_speed_pitotTube(const double pitot_pressure,
 }
 
 int main(int argc, char *argv[]) {
-  struct parameter_uncertainties parameters = {0};
+  struct parameter_uncertainties parameters;
+
+  memset(&parameters, 0, sizeof(struct parameter_uncertainties));
 
   // Load the parameter values.
   load_parameters(&parameters);
