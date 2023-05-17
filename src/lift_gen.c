b35e53ee3481f2cb886cf12a6d9ba9fe1b4dedc1
Vijaya Manohar Dogiparthi <dvijaymanohar@gmail.com>
2023-05-17 19:29:04 +0200

    Added test code

--- src/lift_gen.c
+++ src/lift_gen.c
@@ -217,5 +217,23 @@ int main(int argc, char *argv[]) {
   printf("Lift coefficient: %lf\n", lift_coeff);
   printf("Lift force: %lf N\n", lift_force);
 
+  {
+    double	a, b, c;
+
+    a = libUncertainDoubleUniformDist(0.5, 1.0);
+    printf("a = %lf\n", a);
+
+    b = libUncertainDoubleUniformDist(10.0, 20.0);
+    printf("b = %lf\n", b);
+
+    c = (a+b)/(a-b);
+    printf("c = %lf\n", c);
+
+  #ifdef DEBUG
+    printf("debug message\n");
+  #endif
+
+  }
+
   return 0;
 }
